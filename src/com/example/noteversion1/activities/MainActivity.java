7718187f1f21680a7bac1274fa6663b7f867673c
Sharon D <sharon@mail.com>
2015-04-06 22:27:38 +0300

    WIP added graphic MainActivity

--- src/com/example/noteversion1/activities/MainActivity.java
+++ src/com/example/noteversion1/activities/MainActivity.java
@@ -8,9 +8,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+
 
 import com.example.noteversion1.R;
 import com.example.noteversion1.utils.ConstService;
@@ -20,55 +23,64 @@ public class MainActivity extends Activity {
 
 	MyListView<ShoppingList> myLists;
 	ListView myListsView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		myListsView = (ListView) findViewById(R.id.listViewMyLists);
 		setListenerToList();
-		myLists = new MyListView<ShoppingList>(this, android.R.layout.simple_list_item_1, myListsView);
+		myLists = new MyListView<ShoppingList>(this,
+				android.R.layout.simple_list_item_1, myListsView);
 		getMyLists();
+
+		final Intent intent = new Intent(this, SetListNameActivity.class);
+		ImageView img = (ImageView) findViewById(R.id.image3);
+		img.setClickable(true);
+		img.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(intent);
+			}
+		});
 	}
 
-	public void startCreateListFlow(View view) 
-	{
+	public void startCreateListFlow(View view) {
 		Intent intent = new Intent(this, SetListNameActivity.class);
 		startActivity(intent);
 	}
-	
-	private void getMyLists()
-	{
+
+	private void getMyLists() {
 		GetUserListsById.OnFinishedListener listener = new GetUserListsById.OnFinishedListener() {
-			
+
 			@Override
-			public void onSuccess(List<ShoppingList> list) 
-			{
+			public void onSuccess(List<ShoppingList> list) {
 				for (ShoppingList shoppingList : list) {
 					myLists.add(shoppingList);
 				}
 			}
-			
+
 			@Override
 			public void onError() {
 				// TODO Auto-generated method stub
-				
+
 			}
 		};
-		
-		GetUserListsById task =  new GetUserListsById(listener);
+
+		GetUserListsById task = new GetUserListsById(listener);
 		task.execute("someId");
 	}
-	
-	private void setListenerToList() 
-	{
+
+	private void setListenerToList() {
 		myListsView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
-			{
-				ShoppingList selectedList = (ShoppingList) myListsView.getItemAtPosition(position);
-				Intent intent = new Intent(getApplicationContext(), ShoppingListActivity.class);
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				ShoppingList selectedList = (ShoppingList) myListsView
+						.getItemAtPosition(position);
+				Intent intent = new Intent(getApplicationContext(),
+						ShoppingListActivity.class);
 				intent.putExtra(ConstService.BUNDLE_LIST_ID, selectedList._id);
 				startActivity(intent);
 			}
